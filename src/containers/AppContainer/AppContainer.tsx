8a033eb96a39c007d4444df51d3a5e05745ed668
Ionut Ciucle <ionut.ciucle@gmail.com>
2022-03-09 23:07:50 +0200

    Add AppContainer and basic layout

---
+++ src/containers/AppContainer/AppContainer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AppContainer.scss";
+
+export const AppContainer = () => {
+  return (
+    <div className="dtx__app-container">
+      <header>Welcome to Dorel's File Exploring App!</header>
+      <section className="dtx__app-container__body">
+        <nav>hello</nav>
+        <aside>Hello again</aside>
+      </section>
+    </div>
+  );
+};
+
+export default AppContainer;
