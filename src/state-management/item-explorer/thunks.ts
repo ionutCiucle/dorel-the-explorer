3dbfe560347cb8220177208eca269bfa871d092e
Ionut Ciucle <ionut.ciucle@gmail.com>
2022-07-15 17:13:30 +0300

    Add mockFetch util

--- src/state-management/item-explorer/thunks.ts
+++ src/state-management/item-explorer/thunks.ts
@@ -1,10 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { navItems } from "../../mockData";
+import { mockFetch } from "../../utils";
 
 export const fetchNavigationItems = createAsyncThunk(
   "navigationItems/fetchNavigationItems",
-  () => Promise.resolve(navItems)
-  // new Promise((resolve) => {
-  //   setTimeout(() => resolve(navItems), 300);
-  // })
+  () => mockFetch(navItems)
 );
