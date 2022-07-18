21e0823aedfc8fdd9ac157bf5323f85b71a1de34
Ionut Ciucle <ionut.ciucle@gmail.com>
2022-07-18 20:33:28 +0300

    Add SkeletonLoader multi-line capability

--- src/components/SideNavigation/SkeletonLoader/SkeletonLoader.tsx
+++ src/components/SideNavigation/SkeletonLoader/SkeletonLoader.tsx
@@ -2,12 +2,30 @@ import React from "react";
 import Skeleton from "@mui/material/Skeleton";
 import "./SkeletonLoader.scss";
 
-const SkeletonLoader = () => (
-  <div className="dtx__side-navigation__skeleton-loader">
-    <Skeleton variant="rectangular" width={"80%"} height={30} />
-    <Skeleton variant="rectangular" width={"65%"} height={30} />
-    <Skeleton variant="rectangular" width={"75%"} height={30} />
-  </div>
-);
+type Props = {
+  rows?: number;
+};
+
+const SkeletonLoader = ({ rows = 3 }: Props) => {
+  const renderRows = () => {
+    const rowMarkup = [];
+
+    for (let i = 0; i < rows; i++) {
+      rowMarkup.push(
+        <li key={i}>
+          <Skeleton variant="rectangular" width={"80%"} height={20} />
+          <Skeleton variant="rectangular" width={"65%"} height={20} />
+          <Skeleton variant="rectangular" width={"75%"} height={20} />
+        </li>
+      );
+    }
+
+    return rowMarkup;
+  };
+
+  return (
+    <ul className="dtx__side-navigation__skeleton-loader">{renderRows()}</ul>
+  );
+};
 
 export default SkeletonLoader;
